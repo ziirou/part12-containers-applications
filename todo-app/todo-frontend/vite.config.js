50bff1511487b6476d352c2585b14e88a3750a19
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-09-04 15:31:44 +0300

    Exercise 12.19 - Connect the services, todo-frontend with todo-backend

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
